3cce8dcb4a98d7e231e385f3e7ed23a3e008e7fb
Sergio Lopez Diaz <sldiaz04@gmail.com>
2020-10-05 17:34:22 -0500

    Implemented the Auth Form.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { LoadingController } from '@ionic/angular';
@@ -11,6 +12,8 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLogin = true;
+
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -35,6 +38,27 @@ export class AuthPage implements OnInit {
       loading.dismiss();
       this.router.navigateByUrl('/places/tabs/discover');
     }, 500);
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form);
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to singup servers
+    }
   }
 
 }
